daf921f411aa1c7f0bf8f3855a4969d0d8a9bb60
Croston <21223@nelnet.net>
2017-04-26 21:25:56 -0600

    trying to delete bundle.js

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatService } from "./chat/chat.service";
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    providers: [ChatService]
 })
 export class AppComponent {
     
